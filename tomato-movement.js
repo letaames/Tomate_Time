7019b98fae9631d6431d913999ab21906cb5ecbd
letaames <50257798+letaames@users.noreply.github.com>
2021-10-12 20:19:39 -0700

    Tomato updated

--- tomato-movement.js
+++ tomato-movement.js
@@ -2,23 +2,29 @@ controlMove();
 
 console.log("pom", isPom)
 
-// function controlMove() {
-//     if (isPom === false) {
-//         console.log(isPom);
-//         document.getElementsByClassName("x")[0].style.animationIterationCount = 0;
-//         document.getElementsByClassName("y")[0].style.animationIterationCount = 0;
-//         document.getElementById("tomatoContainer").classList.add("fadeOut");
-//         document.getElementById("tomatoContainer").classList.add("home");
-//         document.getElementById("tomatoContainer").classList.add("fadeIn");
-//     }
-//     else {
-//         document.getElementById("tomatoContainer").classList.add("fadeOut");
-//         document.getElementById("tomatoContainer").classList.remove("home");
-//         document.getElementById("tomatoContainer").classList.add("fadeIn");
+function controlMove() {
+    if (isPom === false) {
+        console.log(isPom);
+        document.getElementById("tomatoSVG").classList.remove("bounce")
+        // document.getElementsByClassName("y")[0].style.animationIterationCount = 0;
+        // document.getElementById("tomatoSVG").classList.remove("scaleDown");
+        // document.getElementById("tomatoSVG").classList.add("home");
+        // document.getElementById("tomatoSVG").classList.add("fadeIn");
+    }
+    else {
+        console.log(isPom);
+        document.getElementById("tomatoSVG").classList.add("bounce")
+        // document.getElementById("tomatoSVG").classList.add("scaleDown");
+        
+        // document.getElementsByClassName("y")[0].style.animationIterationCount = 0;
+        
+//         document.getElementById("tomatoSVG").classList.add("fadeOut");
+//         document.getElementById("tomatoSVG").classList.remove("home");
+//         document.getElementById("tomatoSVG").classList.add("fadeIn");
 
 //         document.getElementsByClassName("x")[0].style.animationIterationCount = "infinite";
 //         document.getElementsByClassName("y")[0].style.animationIterationCount = "infinite";
-//     }
+    }
         
     
 }
