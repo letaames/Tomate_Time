c9a225fd0c12796108bf033d8160dd8db2890169
letaames <50257798+letaames@users.noreply.github.com>
2021-10-20 19:00:42 -0700

    WIP adding slide. Changed set custom to button

--- open.js
+++ open.js
@@ -1,6 +1,7 @@
 document.getElementById("setControls").addEventListener("click", openUp);
 
 function openUp(){
+    // preventDefault();
     if (document.getElementById("section1").style.display !== "grid"){
 
         document.getElementById("section1").style.display = "grid";
@@ -12,4 +13,32 @@ function openUp(){
     }
     
 }
+
+// document.getElementById("setControls").addEventListener("click", function() {
+//     document.getElementsByClassName("container")[0].classList.add('open');
+// }, function() {
+//     // nothing here
+// });
+
+
+// document.getElementById("section1").addEventListener("click", function() {
+//     document.getElementsByClassName("container")[0].classList.remove('open');
+// });
+
+// document.getElementById("setControls").addEventListener("click", openUp);
+
+// function openUp(){
+//     if (document.getElementById("section1").style.display !== "grid"){
+
+//         document.getElementById("section1").style.display = "grid";
+//         document.getElementById("section1").style.height = "auto";
+//         // document.getElementById("section1").classList.add("appear")
+//     }
+//     else {
+//         // document.getElementById("section1").classList.remove("appear");
+//         document.getElementById("section1").style.display = "none";
+//         document.getElementById("section1").style.height ="0";
+//     }
+    
+// }
 
