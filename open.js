13ba70059b68214f3372d60ba5812bd45f4a3f88
letaames <50257798+letaames@users.noreply.github.com>
2021-10-24 21:10:48 -0700

    Merge branch 'main' of https://github.com/letaames/Pomodoro into main

--- open.js
+++ open.js
@@ -1,18 +1,18 @@
-document.getElementById("setControls").addEventListener("click", openUp);
+// document.getElementById("setControls").addEventListener("click", openUp);
 
-function openUp(){
-    // preventDefault();
-    if (document.getElementById("section1").style.display !== "grid"){
+// function openUp(){
+//     // preventDefault();
+//     if (document.getElementById("section1").style.display !== "grid"){
 
-        document.getElementById("section1").style.display = "grid";
-        // document.getElementById("section1").classList.add("appear")
-    }
-    else {
-        // document.getElementById("section1").classList.remove("appear");
-        document.getElementById("section1").style.display = "none";
-    }
+//         document.getElementById("section1").style.display = "grid";
+//         // document.getElementById("section1").classList.add("appear")
+//     }
+//     else {
+//         // document.getElementById("section1").classList.remove("appear");
+//         document.getElementById("section1").style.display = "none";
+//     }
     
-}
+// }
 
 // document.getElementById("setControls").addEventListener("click", function() {
 //     document.getElementsByClassName("container")[0].classList.add('open');
@@ -41,4 +41,42 @@ function openUp(){
 //     }
     
 // }
+
+var linkToggle = document.getElementById("setControls");
+
+linkToggle.addEventListener("click", function(event){
+
+function slideOpen()
+
+    event.preventDefault();
+
+    var container = document.getElementById("section1");
+
+    if (!container.classList.contains('active')) {
+      
+      container.classList.add('active');
+      container.style.height = 'auto';
+
+      var height = container.clientHeight + 'px';
+
+      container.style.height = '0px';
+
+      setTimeout(function () {
+        container.style.height = height;
+      }, 0);
+      
+    } else {
+      
+      container.style.height = '0px';
+
+      container.addEventListener('transitionend', function () {
+        container.classList.remove('active');
+      }, {
+        once: true
+      });
+      
+    }
+    
+  });
+
 
