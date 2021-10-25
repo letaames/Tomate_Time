193dff5dd960a493b8e0c5f64b433fd09f820783
letaames <50257798+letaames@users.noreply.github.com>
2021-10-24 21:07:16 -0700

    Added accordian fold to custom time

--- slide.js
+++ slide.js
@@ -1,2 +1,35 @@
-let target = document.getElementById("section1");
+let linkToggle = document.getElementById("controlBtn");
+
+linkToggle.addEventListener('click', function(event){
+
+    event.preventDefault();
+
+    var container = document.getElementById("section1");
+
+    if (!container.classList.contains('active')) {
+      
+      container.classList.add('active');
+      container.style.height = 'auto';
+
+      var height = container.clientHeight + 'px';
+
+      container.style.height = '0px';
+
+      setTimeout(function () {
+        container.style.height = height;
+      }, 0);
+      
+    } else {
+      
+      container.style.height = '0px';
+
+      container.addEventListener('transitionend', function () {
+        container.classList.remove('active');
+      }, {
+        once: true
+      });
+      
+    }
+    
+  });
 
