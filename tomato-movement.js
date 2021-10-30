b2bac479c4602ecea73fd6b3172e145e8bee9a47
letaames <50257798+letaames@users.noreply.github.com>
2021-10-29 20:09:29 -0700

    Updated tomato animation

--- tomato-movement.js
+++ tomato-movement.js
@@ -4,16 +4,21 @@ controlMove();
 
 function controlMove() {
     if (isPom === false) {
-        // console.log(isPom);
-        document.getElementById("tomatoSVG").classList.remove("bounce")
+        console.log(isPom);
+        // document.getElementById("tomatoSVG").classList.remove("bounce");
+
+        document.getElementById("tomatoSVG").classList.remove("rotate");
         // document.getElementsByClassName("y")[0].style.animationIterationCount = 0;
         // document.getElementById("tomatoSVG").classList.remove("scaleDown");
         // document.getElementById("tomatoSVG").classList.add("home");
         // document.getElementById("tomatoSVG").classList.add("fadeIn");
     }
     else {
-        // console.log(isPom);
-        document.getElementById("tomatoSVG").classList.add("bounce")
+        console.log(isPom);
+        // document.getElementById("tomatoSVG").classList.add("bounce");
+
+        document.getElementById("tomatoSVG").classList.add("rotate");
+
         // document.getElementById("tomatoSVG").classList.add("scaleDown");
         
         // document.getElementsByClassName("y")[0].style.animationIterationCount = 0;
