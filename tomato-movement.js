4fafc84d22b5282e39e63aaf2678632a68cf5118
letaames <50257798+letaames@users.noreply.github.com>
2021-10-29 20:53:35 -0700

    Added new tomato move

--- tomato-movement.js
+++ tomato-movement.js
@@ -6,8 +6,9 @@ function controlMove() {
     if (isPom === false) {
         console.log(isPom);
         // document.getElementById("tomatoSVG").classList.remove("bounce");
-
-        document.getElementById("tomatoSVG").classList.remove("rotate");
+        
+        document.getElementById("tomatoBox").classList.remove("rotate");
+        // document.getElementById("tomatoSVG").classList.remove("scaleDown");
         // document.getElementsByClassName("y")[0].style.animationIterationCount = 0;
         // document.getElementById("tomatoSVG").classList.remove("scaleDown");
         // document.getElementById("tomatoSVG").classList.add("home");
@@ -16,9 +17,10 @@ function controlMove() {
     else {
         console.log(isPom);
         // document.getElementById("tomatoSVG").classList.add("bounce");
-
-        document.getElementById("tomatoSVG").classList.add("rotate");
-
+        
+        document.getElementById("tomatoBox").classList.add("rotate");
+        // document.getElementById("tomatoSVG").classList.add("scaleDown");
+        // document.getElementById("tomatoSVG").style.animationDuration = pomTime + "s";
         // document.getElementById("tomatoSVG").classList.add("scaleDown");
         
         // document.getElementsByClassName("y")[0].style.animationIterationCount = 0;
